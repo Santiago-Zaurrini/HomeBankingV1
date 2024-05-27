33e748ac005940e6865c415873c38a8a0a8050f8
Santiago-Zaurrini <santijz07@gmail.com>
2024-05-27 13:40:51 -0300

    Other inserts

--- Models/DbInitializer.cs
+++ Models/DbInitializer.cs
@@ -11,7 +11,8 @@
                 {
                     new Client { Email = "vcoronado@gmail.com", FirstName="Victor", LastName="Coronado", Password="123456"},
                     new Client { Email = "jperez@gmail.com", FirstName="Juan", LastName="Perez", Password="123457"},
-                    new Client { Email = "mlopez@gmail.com", FirstName="Maria", LastName="Lopez", Password="123458"}
+                    new Client { Email = "mlopez@gmail.com", FirstName="Maria", LastName="Lopez", Password="123458"},
+                    new Client { Email = "szaurrini@gmail.com", FirstName="Santiago", LastName="Zaurrini", Password="31912"}
                 };
 
                 context.Clients.AddRange(clients);
